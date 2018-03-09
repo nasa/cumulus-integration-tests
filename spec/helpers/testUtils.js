22921757777e3adfa3e4c1ac4bc3dc6283342bf0
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-09 18:07:31 -0500

    Use lodash

--- spec/helpers/testUtils.js
+++ spec/helpers/testUtils.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const clonedeep = require('lodash.clonedeep');
+const merge = require('lodash.merge');
 const yaml = require('js-yaml');
 
 function loadConfig() {
@@ -13,7 +15,7 @@ function loadConfig() {
 
 function templateInput({ inputTemplateFilename, config, templatedInputFilename }) {
   const inputTemplate = JSON.parse(fs.readFileSync(inputTemplateFilename));
-  const templatedInput = {...inputTemplate, config};
+  const templatedInput = merge(clonedeep(inputTemplate), config);
   return fs.writeFileSync(templatedInputFilename, JSON.stringify(templatedInput, null, 2));
 };
 
