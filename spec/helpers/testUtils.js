f07da09ee549972fed9dcc5d65ef61d07ea8b8ba
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-13 15:58:59 -0400

    Change config filename

--- spec/helpers/testUtils.js
+++ spec/helpers/testUtils.js
@@ -4,13 +4,13 @@ const merge = require('lodash.merge');
 const yaml = require('js-yaml');
 
 function loadConfig() {
-  let awsConfigFilename = './aws-config.yml';
-  const overrideConfigFilename = './aws-config.override.yml';
+  let configFileName = './spec/config.yml';
+  const overrideConfigFilename = './spec/config.override.yml';
 
-  if (fs.existsSync(overrideConfigFilename)) {
-    awsConfigFilename = overrideConfigFilename;
+  if (fs.existsSync(overrideConfigFilename) || process.env.USE_DEFAULT_CONFIG) {
+    configFileName = overrideConfigFilename;
   }
-  return yaml.safeLoad(fs.readFileSync(awsConfigFilename), 'utf8');
+  return yaml.safeLoad(fs.readFileSync(configFileName), 'utf8');
 };
 
 function templateInput({ inputTemplateFilename, config, templatedInputFilename }) {
