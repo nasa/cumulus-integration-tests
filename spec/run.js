8fb790fa89f08675ca856acbae4c9afb06ded67e
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-14 12:39:08 -0400

    Add babel register

--- spec/run.js
+++ spec/run.js
@@ -1,3 +1,8 @@
+require('babel-register')({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: false
+});
 const Jasmine = require('jasmine');
 
 const jasmine = new Jasmine();
