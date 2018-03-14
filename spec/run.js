0dba13494e85efe5b6d49109a7aef243cb0d4366
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-14 11:02:41 -0400

    Revert babel changes

--- spec/run.js
+++ spec/run.js
@@ -1,8 +1,3 @@
-require('babel-register')({
-  // This will override `node_modules` ignoring - you can alternatively pass
-  // an array of strings to be explicitly matched or a regex / glob
-  ignore: false
-});
 const Jasmine = require('jasmine');
 
 const jasmine = new Jasmine();
