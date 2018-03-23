44533a2cd9526e1226315da6f5fd9063fc71a8c1
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-23 17:43:07 -0400

    Add (duplicate) test and updated configuration

--- spec/helloWorld/HelloWorldSuccessSpec.js
+++ spec/helloWorld/HelloWorldSuccessSpec.js
@@ -32,3 +32,32 @@ describe("The Hello World workflow", function() {
     });
   });
 });
+
+describe("The Hello World workflow using ECS", function() {
+  let workflowExecution = null;
+
+  beforeAll(async function() {
+    workflowExecution = await workflow.executeWorkflow(
+      awsConfig.stackName,
+      awsConfig.bucket,
+      'EcsHelloWorldWorkflow',
+      'spec/helloWorld/HelloWorld.input.json'
+    );
+  });
+
+  it('executes successfully', function() {
+    expect(workflowExecution.status).toEqual('SUCCEEDED');
+  });
+
+  describe("the HelloWorld ECS", function() {
+    let lambdaOutput = null;
+
+    beforeAll(async function() {
+      lambdaOutput = await workflow.getLambdaOutput(workflowExecution.executionArn, "HelloWorld");
+    });
+
+    it("output is Hello World", function() {
+      expect(lambdaOutput.payload).toEqual({ hello: 'Hello World' });
+    });
+  });
+});
